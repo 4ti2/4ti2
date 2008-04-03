c68e7f75ecf5b5c145929a176233930a8efb490f
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2008-04-03 13:01:06 +0000

    Merge from autoconfiscation_branch

--- src/util/output.c
+++ src/util/output.c
@@ -86,7 +86,7 @@ listVector* extractVectorsWithFirstEntryEqualToOne(listVector *basis,
 						   int numOfVars) {
   vector v;
   listVector *tmp, *endBasis;
-
+    
   tmp=basis;
   basis=0;
   endBasis=0;
@@ -127,18 +127,190 @@ listVector* extractInitialForms(listVector *basis, vector w, int numOfVars) {
   return(basis);
 }
 /* ----------------------------------------------------------------- */
+listVector* extractStabilizer(listVector *basis, listVector *S, int numOfVars) {
+  int check;
+  listVector *tmp, *stab, *endStab;
+
+  stab=createListVector(0);
+  endStab=stab;
+
+  while (S) {
+    check=1;
+    tmp=basis;
+    while (tmp) {
+      if (isVectorEqualToPermutedVector(tmp->first,S->first,numOfVars)==0) {
+	tmp=0;
+	check=0;
+      }
+      if (tmp) tmp=tmp->rest;
+    }
+    if (check==1) {
+      endStab->rest=createListVector(S->first);
+      endStab=endStab->rest;
+    }
+    S=S->rest;
+  }
+
+  return(stab->rest);
+}
+/* ----------------------------------------------------------------- */
+int isVectorFixed(vector v, vector fix, int numOfFixedPoints) {
+  int i;
+
+  for (i=0;i<numOfFixedPoints;i++)
+    if (v[fix[i]]!=fix[i]) return (0);
+
+  return (1);
+}
+/* ----------------------------------------------------------------- */
+int isVectorRelaxedFixed(vector v, vector fix, int numOfFixedPoints) {
+  int i,j,ok;
+
+  for (i=0;i<numOfFixedPoints;i++) {
+    ok=0;
+    for (j=0;j<numOfFixedPoints;j++) if (v[fix[i]]==fix[j]) ok=1;
+    if (ok==0) return (0);
+  }
+  return (1);
+}
+/* ----------------------------------------------------------------- */
+listVector* extractFixedVectors(listVector *basis, vector fix, 
+				int numOfFixedPoints) {
+  listVector *F, *endF;
+
+  F=createListVector(0);
+  endF=F;
+
+  while (basis) {
+    if (isVectorFixed(basis->first,fix,numOfFixedPoints)==1) {
+      endF->rest=createListVector(basis->first);
+      endF=endF->rest;
+    }
+    basis=basis->rest;
+  }
+
+  return(F->rest);
+}
+/* ----------------------------------------------------------------- */
+listVector* extractRelaxedFixedVectors(listVector *basis, vector fix, 
+				       int numOfFixedPoints) {
+  listVector *F, *endF;
+
+  F=createListVector(0);
+  endF=F;
+
+  while (basis) {
+    if (isVectorRelaxedFixed(basis->first,fix,numOfFixedPoints)==1) {
+      endF->rest=createListVector(basis->first);
+      endF=endF->rest;
+    }
+    basis=basis->rest;
+  }
+
+  return(F->rest);
+}
+/* ----------------------------------------------------------------- */
+int isVectorDominatedByVector(vector v, vector w, int numOfVars) {
+  int i;
+
+  for (i=0; i<numOfVars; i++) {
+    if (v[i]>w[i]) return (0);     
+  }
+  return (1);
+}
+/* ----------------------------------------------------------------- */
+int isVectorDominatedByListVector(vector v, listVector *dom, int numOfVars) {
+
+
+  while (dom) {
+    if (isVectorDominatedByVector(v,dom->first,numOfVars)==1) return (1);
+    dom=dom->rest;
+  }
+
+  return (0);
+}
+/* ----------------------------------------------------------------- */
+listVector* extractNonDominatedVectors(listVector *basis, listVector *dom, 
+				       int numOfVars) {
+  int lenBasis, count;
+  listVector *F, *endF, *tmp;
+
+  F=createListVector(0);
+  endF=F;
+
+  lenBasis=lengthListVector(basis);
+  count=0;
+
+  while (basis) {
+    count ++;
+    if (count==(1000*(count/1000))) 
+      printf("Considering vector %d/%d\n",count,lenBasis);
+    if (isVectorDominatedByListVector(basis->first,dom,numOfVars)==0) {
+      endF->rest=createListVector(basis->first);
+      endF=endF->rest;
+    }
+    tmp=basis;
+    basis=basis->rest;
+    free(tmp);
+  }
+
+  return(F->rest);
+}
+/* ----------------------------------------------------------------- */
+listVector* extractMaximalNonDominatedVectors(listVector *basis, 
+					      listVector *symmGroup,
+					      int numOfVars) {
+  int count, maxNorm;
+  vector maxVector;
+  listVector *tmp, *M, *endM, *orbit;
+
+  M=createListVector(0);
+  endM=M;
+  maxVector=0;
+
+  count=0;
+  printf("%d nondominated vectors found, %d vectors left to consider\n",
+	 count,lengthListVector(basis));
+
+  while (basis) {
+    maxNorm=maximalNormInListVector(basis,numOfVars);
+    tmp=basis;
+    while (tmp) {
+      if (normOfVector(tmp->first,numOfVars)==maxNorm) {
+	count++;
+	maxVector=tmp->first;
+	endM->rest=createListVector(maxVector);
+	endM=endM->rest;
+	tmp=0;
+      } else {
+	tmp=tmp->rest;
+      }
+    }
+
+    orbit=expandRepresentativeIntoFullOrbits(createListVector(maxVector),
+					     symmGroup,numOfVars,10); 
+
+    basis=extractNonDominatedVectors(basis,orbit,numOfVars);
+    printf("%d nondominated vectors found, %d vectors left to consider\n",
+	   count,lengthListVector(basis));
+  }
+
+  return(M->rest);
+}
+/* ----------------------------------------------------------------- */
 int output_main(int argc, char *argv[]) {
-  int x,y,z,numOfVars,numOfRows, numOfLabels,infoLevel,degree,coord;
+  int i,j,x,y,z,numOfVars,numOfRows,numOfFixPoints,numOfLabels,infoLevel,
+    degree,lowdegree,highdegree,coord,sizeOfLayer,val;
   char *s;
-  char fileName[127],outFileName[127],symFileName[127],varFileName[127],
-    groFileName[127],costFileName[127];
+  char fileName[127],outFileName[127],domFileName[127],symFileName[127],
+    varFileName[127],groFileName[127],costFileName[127];
   char **labels;
-  vector v,w;
-  listVector *basis, *tmpV, *symmGroup, *weights;
+  vector v,w,fixpoints;
+  listVector *A, *B, *C, *basis, *domBasis, *tmp, *tmpV, *symmGroup, *weights;
   FILE *in;
 
   infoLevel=standardInfoLevel;
-  for (int i=1; i<argc-1; i++) {
+  for (i=1;i<argc-1;i++) {
     if (strncmp(argv[i],"--",2)==0) {
       if (strncmp(argv[i],"--qui",5)==0) {
 	infoLevel=-1;
@@ -189,6 +361,52 @@ if (infoLevel>-1) {
       strcpy(outFileName,fileName);
       strcat(outFileName,".rep");
       printListVectorToFile(outFileName,basis,numOfVars);
+      printf("%d representatives found.\n",lengthListVector(basis));
+    }
+    if (strncmp(argv[i],"--dom",5)==0) {
+      strcpy(fileName,argv[argc-1]);
+      basis=readListVector(&numOfVars,fileName);
+      strcpy(domFileName,argv[argc-2]);
+      domBasis=readListVector(&numOfVars,domFileName);
+
+      basis=extractNonDominatedVectors(basis,domBasis,numOfVars); 
+      strcpy(outFileName,fileName);
+      strcat(outFileName,".nondom");
+      printListVectorToFile(outFileName,basis,numOfVars);
+      printf("%d non-dominated vectors found.\n",
+	     lengthListVector(basis));
+    }
+    if (strncmp(argv[i],"--max",5)==0) {
+      strcpy(fileName,argv[argc-1]);
+      basis=readListVector(&numOfVars,fileName);
+      strcpy(symFileName,fileName);
+      strcat(symFileName,".sym.full");
+      symmGroup=readListVector(&numOfVars,symFileName);
+
+      if (symmGroup==0) { 
+        strcpy(fileName,argv[argc-1]);
+        basis=readListVector(&numOfVars,fileName);
+	strcpy(symFileName,fileName);
+	strcat(symFileName,".sym");
+	symmGroup=readListVector(&numOfVars,symFileName);
+
+	tmpV=symmGroup;
+	while (tmpV) {
+	  v=tmpV->first;
+	  for (int i=0; i<numOfVars; i++) v[i]=v[i]-1;
+	  tmpV->first=v;
+	  tmpV=tmpV->rest;
+	}
+	symmGroup=generateSymmetryGroup(symmGroup,numOfVars);
+	strcat(symFileName,".full");
+	printListVectorToFile(symFileName, symmGroup, numOfVars);
+      }
+      basis=extractMaximalNonDominatedVectors(basis,symmGroup,numOfVars); 
+      strcpy(outFileName,fileName);
+      strcat(outFileName,".maxnondom");
+      printListVectorToFile(outFileName,basis,numOfVars);
+      printf("%d maximal non-dominated vectors found.\n",
+	     lengthListVector(basis));
     }
     if (strncmp(argv[i],"--exp",5)==0) {
       strcpy(fileName,argv[argc-1]);
@@ -221,14 +439,63 @@ if (infoLevel>-1) {
       if (argc==3) {
 	printL1NormOfListVector(basis,numOfVars);
       } else {
-	degree=atoi(argv[i+1]);
-	printf("degree = %d\n",degree);
-	strcpy(outFileName,fileName);
-	strcat(outFileName,".deg.");
-	strcat(outFileName,argv[i+1]);
-	printListVectorWithGivenDegreeToFile(outFileName,basis,numOfVars,
-					     degree);
+	if (argc==4) {
+	  degree=atoi(argv[i+1]);
+	  printf("degree = %d\n",degree);
+	  strcpy(outFileName,fileName);
+	  strcat(outFileName,".deg.");
+	  strcat(outFileName,argv[i+1]);
+	  printListVectorWithGivenDegreesToFile(outFileName,basis,numOfVars,
+						degree,degree);
+	} else {
+	  lowdegree=atoi(argv[i+1]);
+	  highdegree=atoi(argv[i+2]);
+	  printf("degrees %d..%d\n",lowdegree,highdegree);
+	  strcpy(outFileName,fileName);
+	  strcat(outFileName,".deg.");
+	  strcat(outFileName,argv[i+1]);
+	  strcat(outFileName,"-");
+	  strcat(outFileName,argv[i+2]);
+	  printListVectorWithGivenDegreesToFile(outFileName,basis,numOfVars,
+						lowdegree,highdegree);
+	}
       }
+      return(0);
+    }
+    if (strncmp(argv[i],"--sup",5)==0) {
+      strcpy(fileName,argv[argc-1]);
+      basis=readListVector(&numOfVars,fileName);
+      if (argc==3) {
+	printSupportsOfListVector(basis,numOfVars);
+      } else {
+	if (argc==4) {
+	  degree=atoi(argv[i+1]);
+	  printf("size of support = %d\n",degree);
+	  strcpy(outFileName,fileName);
+	  strcat(outFileName,".supp.");
+	  strcat(outFileName,argv[i+1]);
+	  printListVectorWithGivenSupportsToFile(outFileName,basis,numOfVars,
+						 degree,degree);
+	} else {
+	  lowdegree=atoi(argv[i+1]);
+	  highdegree=atoi(argv[i+2]);
+	  printf("sizes of support %d..%d\n",lowdegree,highdegree);
+	  strcpy(outFileName,fileName);
+	  strcat(outFileName,".supp.");
+	  strcat(outFileName,argv[i+1]);
+	  strcat(outFileName,"-");
+	  strcat(outFileName,argv[i+2]);
+	  printListVectorWithGivenSupportsToFile(outFileName,basis,numOfVars,
+						 lowdegree,highdegree);
+	}
+      }
+      return(0);
+    }
+    if (strncmp(argv[i],"--typ",5)==0) {
+      strcpy(fileName,argv[argc-1]);
+      basis=readListVector(&numOfVars,fileName);
+      sizeOfLayer=atoi(argv[i+1]);
+      printTypesOfListVector(basis,sizeOfLayer,numOfVars);
       return(0);
     }
     if (strncmp(argv[i],"--non",5)==0) {
@@ -246,6 +513,16 @@ if (infoLevel>-1) {
 						   numOfVars,coord);
       }
       return(0);
+    }
+    if (strncmp(argv[i],"--AxB",5)==0) {
+      strcpy(fileName,argv[argc-3]);
+      A=readListVector(&numOfVars,fileName);
+      strcpy(fileName,argv[argc-2]);
+      B=readListVector(&numOfVars,fileName);
+      v=matrixTimesVector(A,B->first,lengthListVector(A),numOfVars);
+      C=createListVector(v);
+      strcpy(fileName,argv[argc-1]);
+      printListVectorToFile(fileName,C,lengthListVector(A));
     }
     if (strncmp(argv[i],"--0-1",5)==0) {
       strcpy(fileName,argv[argc-1]);
@@ -333,6 +610,103 @@ if (infoLevel>-1) {
 	fclose(in);
       }
       printListBinomialsToFile(outFileName,basis,numOfVars,labels);
+    }
+    if (strncmp(argv[i],"--sum",5)==0) {
+      strcpy(fileName,argv[argc-1]);
+      basis=readListVector(&numOfVars,fileName);
+      v=createVector(numOfVars);
+      for (i=0; i<numOfVars; i++) v[i]=0;
+      while (basis) {
+	for (i=0; i<numOfVars; i++) v[i]=v[i]+(basis->first)[i];
+	basis=basis->rest;
+      }
+      printVector(v,numOfVars);
+    }
+    if (strncmp(argv[i],"--sub",5)==0) {
+      strcpy(fileName,argv[argc-2]);
+      basis=readListVector(&numOfVars,fileName);
+      v=basis->first;
+      strcpy(fileName,argv[argc-1]);
+      basis=readListVector(&numOfVars,fileName);
+      strcpy(outFileName,fileName);
+      strcat(outFileName,".submat");
+      printSubsetOfListVectorToFile(outFileName,basis,v,numOfVars);
+    }
+    if (strncmp(argv[i],"--rem",5)==0) {
+      if (strncmp(argv[i],"--remcol",8)==0) {
+	/* Remove column. */
+        strcpy(fileName,argv[argc-1]);
+        basis=readListVector(&numOfVars,fileName);
+	coord=atoi(argv[argc-2]);
+        strcpy(outFileName,fileName);
+        strcat(outFileName,".remcol");
+        printListVectorWithoutColumnToFile(outFileName,basis,coord,numOfVars);
+      }
+    }
+    if (strncmp(argv[i],"--sta",5)==0) {
+      /* Extracting those symmetries from a list of given symmetries 
+	 that keep a given list of vectors fixed. */
+      strcpy(fileName,argv[argc-1]);
+      basis=readListVector(&numOfVars,fileName);
+      strcpy(symFileName,argv[argc-2]);
+      symmGroup=readListVector(&numOfVars,symFileName);
+      symmGroup=extractStabilizer(basis,symmGroup,numOfVars);
+      strcpy(outFileName,symFileName);
+      strcat(outFileName,".stab");
+      printListVectorToFile(outFileName,symmGroup,numOfVars);
+    }
+    if (strncmp(argv[i],"--fil",5)==0) {
+      /* Fill specified column with specified value. */
+
+      strcpy(fileName,argv[argc-1]);
+      basis=readListVector(&numOfVars,fileName);
+      coord=atoi(argv[argc-3]);
+      val=atoi(argv[argc-2]);
+      tmp=basis;
+      while (tmp) {
+	(tmp->first)[coord-1]=val;
+	tmp=tmp->rest;
+      }
+      strcpy(outFileName,fileName);
+      strcat(outFileName,".fil");
+      printListVectorToFile(outFileName,basis,numOfVars);
+    }
+    if (strncmp(argv[i],"--add",5)==0) {
+      /* Add column with specified value. */
+        strcpy(fileName,argv[argc-1]);
+        basis=readListVector(&numOfVars,fileName);
+	coord=atoi(argv[argc-3]);
+	val=atoi(argv[argc-2]);
+        strcpy(outFileName,fileName);
+        strcat(outFileName,".addcol");
+        printListVectorWithAdditionalColumnToFile(outFileName,basis,
+						 coord,val,numOfVars);
+    }
+    if (strncmp(argv[i],"--fix",5)==0) {
+      /* Extracting those vectors that have given coordinates x[i]=i. */
+
+      numOfFixPoints=argc-3;
+      fixpoints=createVector(numOfFixPoints);
+      for (j=2;j<argc-1;j++) fixpoints[j-2]=atoi(argv[j]);
+      strcpy(fileName,argv[argc-1]);
+      basis=readListVector(&numOfVars,fileName);
+      basis=extractFixedVectors(basis,fixpoints,numOfFixPoints);
+      strcpy(outFileName,fileName);
+      strcat(outFileName,".fix");
+      printListVectorToFile(outFileName,basis,numOfVars);
+    }
+    if (strncmp(argv[i],"--fox",5)==0) {
+      /* Extracting those vectors that have given coordinates x[i]=i. */
+
+      numOfFixPoints=argc-3;
+      fixpoints=createVector(numOfFixPoints);
+      for (j=2;j<argc-1;j++) fixpoints[j-2]=atoi(argv[j]);
+      strcpy(fileName,argv[argc-1]);
+      basis=readListVector(&numOfVars,fileName);
+      basis=extractRelaxedFixedVectors(basis,fixpoints,numOfFixPoints);
+      strcpy(outFileName,fileName);
+      strcat(outFileName,".fox");
+      printListVectorToFile(outFileName,basis,numOfVars);
     }
     if (strncmp(argv[i],"--ini",5)==0) {
       strcpy(fileName,argv[argc-1]);
