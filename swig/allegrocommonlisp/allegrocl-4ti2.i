4bd9e7da6a8df43e3d239cf60079e9613f659d8b
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2008-09-10 03:16:23 +0000

    More infrastructure for the higher-level-language interfaces.

--- swig/allegrocommonlisp/allegrocl-4ti2.i
+++ swig/allegrocommonlisp/allegrocl-4ti2.i
@@ -2,4 +2,12 @@
 
 %module "4ti2";
 
+typedef int int32_t ;
+typedef long int int64_t;
+
+%{
+#include "4ti2/4ti2.h"
+%}
+
+%include "4ti2/4ti2.h"
 
---
+++ swig/allegrocommonlisp/examples/example.lisp
@@ -0,0 +1,6 @@
+(use-package :4ti2)
+
+(let* ((state (_4ti2_rays_create_state _4ti2_prec_int_64)))
+
+;; TODO: Lispify identifiers
+;;       Typemaps for errors and indirect returns
