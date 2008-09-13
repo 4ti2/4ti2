c9411e2a7ffce85cbff64ebd8fee26a216114116
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2008-09-13 23:14:06 +0000

    *** empty log message ***

--- swig/allegrocommonlisp/allegrocl-4ti2.i
+++ swig/allegrocommonlisp/allegrocl-4ti2.i
@@ -2,12 +2,37 @@
 
 %module "4ti2";
 
-typedef int int32_t ;
+%include "pointer-in-out.i"
+%include "list-vector.i"
+
+typedef int int32_t;
 typedef long int int64_t;
 
 %{
 #include "4ti2/4ti2.h"
 %}
+
+%inline %{
+
+struct _4ti2_state {};
+struct _4ti2_matrix {};
+
+%}
+
+%typemap(lout) _4ti2_status
+%{ (let ((status $body))
+     (check-4ti2-status status)) %}
+
+TYPEMAP_POINTER_INPUT_OUTPUT(_4ti2_state *, -4ti2-state)
+%apply _4ti2_state** OUTPUT { _4ti2_state** state };
+
+TYPEMAP_POINTER_INPUT_OUTPUT(_4ti2_matrix *, -4ti2-matrix)
+%apply _4ti2_matrix** OUTPUT { _4ti2_matrix** matrix };
+
+%apply int *OUTPUT { int32_t *value };
+
+%apply int PARALLEL_LISTLENINPUT { int argc };
+%apply char **PARALLEL_LISTINPUT { char **argv };
 
 %include "4ti2/4ti2.h"
 
--- swig/allegrocommonlisp/examples/example.lisp
+++ swig/allegrocommonlisp/examples/example.lisp
@@ -1,6 +1,43 @@
 (use-package :4ti2)
 
-(let* ((state (_4ti2_rays_create_state _4ti2_prec_int_64)))
+(let* ((state (-4ti2-rays-create-state *-4ti2-prec-int-32*))
+       (mat (-4ti2-state-create-matrix state 
+				       #|numrows:|# 2 
+						    #|numcols:|# 3
+								 "mat"))
+       (rel (-4ti2-state-create-matrix state 1 2 "rel")))
+  (-4ti2-matrix-set-entry-int32-t mat 0 2 47)
+  (-4ti2-matrix-write-to-stdout mat)
+  (-4ti2-matrix-write-to-stdout rel)
+  (-4ti2-state-set-options state '("IGNORED-ARG" "--support"))
+  (-4ti2-state-compute state)
+  (let ((hom (-4ti2-state-get-matrix state "qhom")))
+    ;;(-4ti2-matrix-write-to-stdout hom)
+    (format t "Numrows: ~D Numcols: ~D~%" 
+	    (-4ti2-matrix-get-num-rows hom)
+	    (-4ti2-matrix-get-num-cols hom))
+    )
+  (-4ti2-state-delete state))
 
-;; TODO: Lispify identifiers
-;;       Typemaps for errors and indirect returns
+(let* ((state (-4ti2-rays-create-state *-4ti2-prec-int-arb*))
+       ;; FIXME: Non-implemented precision does not signal an error!
+       (mat (-4ti2-state-create-matrix state 
+				       #|numrows:|# 2 
+						    #|numcols:|# 3
+								 "mat"))
+       (rel (-4ti2-state-create-matrix state 1 2 "rel")))
+  (-4ti2-matrix-set-entry-int32-t mat 0 2 47)
+  (-4ti2-matrix-write-to-stdout mat)
+  (-4ti2-matrix-write-to-stdout rel)
+  (-4ti2-state-compute state)
+  (let ((hom (-4ti2-state-get-matrix state "qhom")))
+    (-4ti2-matrix-write-to-stdout hom))
+  (-4ti2-state-delete state))
+
+;;; Error handling:
+
+(let* ((state (-4ti2-rays-create-state *-4ti2-prec-int-32*))
+       (xyz (-4ti2-state-create-matrix state 1 2 "xyz")))
+  ;; FIXME: Does not signal an error!!
+  nil)
+
