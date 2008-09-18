f88704c2c4258afab4e8045188021294fe4e164c
Peter Malkin <malkin@math.ucdavis.edu>
2008-09-18 06:41:22 +0000

    I have moved groebner/4ti2API.h to 4ti2/4ti2xx.h.

--- src/groebner/VectorArrayAPI.h
+++ src/groebner/VectorArrayAPI.h
@@ -23,7 +23,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #ifndef _4ti2_groebner__VectorArrayAPI_
 #define _4ti2_groebner__VectorArrayAPI_
 
-#include "groebner/4ti2API.h"
+#include "4ti2/4ti2xx.h"
 #include "groebner/VectorArray.h"
 
 namespace _4ti2_ {
