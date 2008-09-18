00fcf5ea72b306219c27175ee722a951880abeb2
Peter Malkin <malkin@math.ucdavis.edu>
2008-09-18 06:40:13 +0000

    I have moved the file groebner/4ti2API.cpp to 4ti2/4ti2xx.h

--- src/groebner/4ti2API.cpp
+++ src/groebner/4ti2API.cpp
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
 #include "4ti2/4ti2.h"
-#include "groebner/4ti2API.h"
+#include "4ti2/4ti2xx.h"
 #include "groebner/QSolveAPI.h"
 #include "groebner/RaysAPI.h"
 #include "groebner/CircuitsAPI.h"
