575810c54fee96c64edec12f42ea84418599f921
Peter Malkin <malkin@math.ucdavis.edu>
2008-11-05 17:32:02 +0000

    This file checks the QSolve API.  It checks inputting a simple example using api calls other than file input calls.

--- test/qsolve/api/qsolve_main.cpp
+++ test/qsolve/api/qsolve_main.cpp
@@ -22,24 +22,80 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <cstring>
 
-#include "groebner/QSolveAPI.h"
-#include "groebner/RaysAPI.h"
-#include "groebner/CircuitsAPI.h"
-
-using namespace _4ti2_;
+#include "4ti2/4ti2.h"
+#include "4ti2/4ti2xx.h"
 
 int
-main(int argc, char **argv)
+main()
 {
-    QSolveAPI* qsolve_api = 0;
-    qsolve_api = new QSolveAPI;
-    qsolve_api->read("ignore");
-    qsolve_api->compute();
-    qsolve_api->write("ignore");
+    // Input data.
+    const int m = 4;
+    const int n = 3;
+    int64_t mat[m][n] = {
+                { 2,  3, -6 },
+                { 2, -1, -4 },
+                { 1,  2, -11 },
+                { 1, -1,  1 }
+            };
+    int64_t rel[m] = { _4ti2_LB, _4ti2_UB, _4ti2_UB, _4ti2_LB };
+    int64_t sign[n] = { _4ti2_LB, _4ti2_LB, _4ti2_LB };
 
-    delete qsolve_api;
+    // Output data.
+    const int k = 4;
+    int64_t qhom[k][n] = {
+                { 3, 4, 1 },
+                { 3, 8, 5 },
+                { 9, 2, 4 },
+                {19,18, 5 }
+            };
+
+    _4ti2_state* qsolve_api = _4ti2_qsolve_create_state(_4ti2_PREC_INT_64);
+    const int argc = 2;
+    char*argv[2] = { "qsolve", "--quiet" };
+    _4ti2_state_set_options(qsolve_api, argc, argv);
+
+    _4ti2_matrix* cons_matrix;
+    _4ti2_state_create_matrix(qsolve_api, m, n, "mat", &cons_matrix);
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            _4ti2_matrix_set_entry_int64_t(cons_matrix, i, j,  mat[i][j]);
+        }
+    }
+    //_4ti2_matrix_write_to_stdout(cons_matrix);
+
+    _4ti2_matrix* rel_matrix;
+    _4ti2_state_create_matrix(qsolve_api, 1, m, "rel", &rel_matrix);
+    for (int i = 0; i < m; ++i) {
+        _4ti2_matrix_set_entry_int64_t(rel_matrix, 0, i, rel[i]);
+    }
+    //_4ti2_matrix_write_to_stdout(rel_matrix);
+
+    _4ti2_matrix* sign_matrix;
+    _4ti2_state_create_matrix(qsolve_api, 1, n, "sign", &sign_matrix);
+    for (int i = 0; i < n; ++i) {
+        _4ti2_matrix_set_entry_int64_t(sign_matrix, 0, i, sign[i]);
+    }
+    //_4ti2_matrix_write_to_stdout(sign_matrix);
+
+    _4ti2_state_compute(qsolve_api);
+
+    _4ti2_matrix* qhom_matrix;
+    _4ti2_state_get_matrix(qsolve_api, "qhom", &qhom_matrix);
+    //_4ti2_matrix_write_to_stdout(qhom_matrix);
+
+    // Check the output
+    if (_4ti2_matrix_get_num_rows(qhom_matrix) != k) { return 1; }
+    if (_4ti2_matrix_get_num_cols(qhom_matrix) != n) { return 1; }
+    for (int i = 0; i < k; ++i) {
+        for (int j = 0; j < n; ++j) {
+            int64_t value;
+            _4ti2_matrix_get_entry_int64_t(qhom_matrix, i, j, &value);
+            if (value != qhom[i][j]) { return 1; }
+        }
+    }
+
+    _4ti2_state_delete(qsolve_api);
     return 0;
 }   
+
