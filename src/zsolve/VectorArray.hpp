4eb972cf9133d3569b62cd570e9957987d31ba84
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2014-01-15 01:21:28 +0000

    Compile fix for XCode 5.0.2, Apple LLVM version 5.0 (clang-500.2.79).

--- src/zsolve/VectorArray.hpp
+++ src/zsolve/VectorArray.hpp
@@ -26,6 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <fstream>
 
 #include "zsolve/Integer.h"
 #include "zsolve/Vector.hpp"
