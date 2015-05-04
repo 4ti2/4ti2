75c4500a63ffd1bda1eb4c63ae5d151116e796d2
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2015-05-04 03:18:23 +0000

    Remove abandoned 'new' qsolve implementation from source tree.  It is preserved in branch 'qsolve', which has further changes from August 2010 that were not merged to trunk.

--- src/qsolve/ConeStream.h
+++
@@ -1,8 +0,0 @@
-#ifndef _4ti2_qsolve__ConeStream_
-#define _4ti2_qsolve__ConeStream_
-
-#include "Cone.h"
-
-// Input and output functions for Cone.
-
-#endif
--- src/qsolve/MatrixStream.h
+++
@@ -1,7 +0,0 @@
-#ifndef _4ti2_qsolve__MatrixStream_
-#define _4ti2_qsolve__MatrixStream_
-
-#include "VectorArrayStream.h"
-#include "Matrix.h"
-
-#endif
