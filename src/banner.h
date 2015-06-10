52ce40ed6713f62f838bce6889d72c29c928b967
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2015-06-10 17:49:55 +0000

    Update copyright years

--- src/banner.h
+++ src/banner.h
@@ -4,7 +4,8 @@
 
 #define FORTY_TWO_BANNER \
 "-------------------------------------------------\n\
-4ti2 version " PACKAGE_VERSION ", Copyright 2006,2015 4ti2 team.\n\
+4ti2 version " PACKAGE_VERSION "\n\
+Copyright 1998, 2002, 2006, 2015 4ti2 team.\n\
 4ti2 comes with ABSOLUTELY NO WARRANTY.\n\
 This is free software, and you are welcome\n\
 to redistribute it under certain conditions.\n\
