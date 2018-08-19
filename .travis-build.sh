3550d7887705490be150559943cf3647de03e274
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2018-08-19 22:20:26 +0200

    .travis-build.sh: Use make distcheck

--- .travis-build.sh
+++ .travis-build.sh
@@ -5,7 +5,7 @@ case x$DOCKER in
     x)
 	mkdir -p build
 	cd build
-	../configure --srcdir=.. && make -j2 && make -j2 check
+	../configure --srcdir=.. && make -j2 distcheck
 	;;
     *i386*)
 	docker run -i -v "${PWD}:/src" $DOCKER /bin/bash -c "linux32 --32bit i386 /src/.docker-build.sh"
