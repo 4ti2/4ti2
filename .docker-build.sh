c61dc7971e5e36bbb5f9a55011dde4164fe1acaf
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2018-08-23 15:37:45 +0200

    Remove autotools from Docker build

--- .docker-build.sh
+++ .docker-build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -e
-yum install -y gmp-devel gcc gcc-c++ glpk-devel || ( apt-get update && apt-get -y install g++ libgmp-dev libglpk-dev autoconf automake libtool make)
+yum install -y gmp-devel gcc gcc-c++ glpk-devel || ( apt-get update && apt-get -y install g++ libgmp-dev libglpk-dev make)
 mkdir -p /build
 cd /build
 /src/configure --srcdir=/src  || ( echo '#### Contents of config.log: ####'; cat config.log; exit 1)
